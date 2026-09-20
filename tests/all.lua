25280a08fe4725d0226c3ab7db39256bc149da6e
gongahkia <angryapplegravy@gmail.com>
2026-09-20 12:32:27 +0800

    addedideas

--- tests/all.lua
+++ tests/all.lua
@@ -1,8 +1,8 @@
 local T={}
 function T.run()
  local a=require('tests.suite').run(true);local b=require('tests.maps').run()
- local c=require('tests.expansion').run();local d=require('tests.campaign').run();local e=require('tests.region').run();local f=require('tests.logistics').run();local g=require('tests.travel').run();local h=require('tests.knowledge').run()
- local result={groups=a.groups+b.groups+c.groups+d.groups+e.groups+f.groups+g.groups+h.groups,assertions=a.assertions+b.assertions+c.assertions+d.assertions+e.assertions+f.assertions+g.assertions+h.assertions}
+ local c=require('tests.expansion').run();local d=require('tests.campaign').run();local e=require('tests.region').run();local f=require('tests.logistics').run();local g=require('tests.travel').run();local h=require('tests.knowledge').run();local i=require('tests.education').run()
+ local result={groups=a.groups+b.groups+c.groups+d.groups+e.groups+f.groups+g.groups+h.groups+i.groups,assertions=a.assertions+b.assertions+c.assertions+d.assertions+e.assertions+f.assertions+g.assertions+h.assertions+i.assertions}
  print('TOTAL: '..result.groups..' groups; '..result.assertions..' assertions.')
  return result
 end
